6155edb622b2ba418528aea7dcd15c145e528461
ibrahimbas8 <49797348+ibrahimbas8@users.noreply.github.com>
2021-08-28 18:24:58 +0300

    4. soru duzeltıldı

--- homework12.sql
+++ homework12.sql
@@ -36,4 +36,3 @@ FROM payment
 INNER JOIN customer ON customer.customer_id = payment.customer_id
 GROUP BY payment.customer_id, customer.first_name, customer.last_name
 ORDER BY COUNT(amount) DESC
-LIMIT 1;
